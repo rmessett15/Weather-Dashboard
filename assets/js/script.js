7947e678bdb6c7fdce25fa23fe1174955446562c
rmessett15 <rmessett15@gmail.com>
2023-03-17 18:32:16 -0500

    added JSON.parse to local data, and added 3 functions to script.js (1. setCurrentWeather() 2. setForecast() 3. setForecastDay(), and modified classes and id's within index.html file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+// tries to grab values from local storage and if there is no data there JSON parse will fail, and it will use an empty list instead as backup
+var recentSearches = JSON.parse(localStorage.getItem("searches")) || [];
+console.log(recentSearches);
+
 function getWeather(city) {
     var url = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=011d8600955301988250a993be42df9e";
     fetch(url)
@@ -7,8 +11,40 @@ function getWeather(city) {
     .then(function (data) {
         console.log(data);
         localStorage.setItem("city", JSON.stringify(data));
+        setCurrentWeather(data.list[0]);
+        setForecast(data.list);
         // localStorage.getItem("city", JSON.parse(data));????
     })
+}
+
+function setCurrentWeather(weather) {
+    console.log(weather.main.temp);
+    var currentTemperature = document.getElementById("current-temperature")
+    currentTemperature.textContent = "Temperature: " + weather.main.temp;
+    console.log(weather.main.humidity);
+}
+
+function setForecast(forecast) {
+    for (var i = 0; i < forecast.length; i += 8) {
+        console.log(forecast[i]);
+        setForecastDay(forecast[i], (i/8) + 1);
+    }
+}
+
+function setForecastDay(weather, dayNumber) {
+    // 1. Get the div element for the day (using ID)
+    // 2. Create an array for the new elements that you will add to the day
+    // 3. For each part of the weather, create a new element for it. This involves creating the element, and setting the text
+    // 4. Replace the existing children in the day element with the new children
+    var day = document.getElementById("day-" + dayNumber)
+    var dayList = [];
+    var titleParagraph = document.createElement("p");
+    titleParagraph.textContent = "Day" + dayNumber;
+    dayList.push(titleParagraph);
+    var tempParagraph = document.createElement("p");
+    tempParagraph.textContent = "Temperature: " + weather.main.temp;
+    dayList.push(tempParagraph);
+    day.replaceChildren(...dayList);
 }
 
 var searchForm = document.getElementById("search-form");
@@ -16,6 +52,8 @@ var city = document.getElementById("city");
 searchForm.addEventListener("submit", function(event) {
     event.preventDefault();
     var searchCity = city.value.trim()
+    recentSearches.push(searchCity);
+    localStorage.setItem("searches", JSON.stringify(recentSearches));
     getWeather(searchCity);
 });
 
