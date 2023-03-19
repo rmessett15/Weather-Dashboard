4819b6768d10570169c3547e234035a1829735cd
rmessett15 <rmessett15@gmail.com>
2023-03-19 17:44:52 -0500

    added buttons to recent search history that a clickable and can return locally stored data, added icon, and date to 5 day forecast, added title to README.md, and made small adjustments based on JS file needs to index.html

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# Weather-Dashboard
-Week-6-Challenge (Server-side APIs)
+# <Creating Weather Dashboard: Week-6-Challenge (Server-side APIs)>
+
+## Description
+
+The idea behind this project was to create a work day scheduler so that users can add events to specific hour time blocks to help them plan out their day between business hours (9AM-5PM). It allows users to dynamically update and add to their schedule through the use of JavaScript. Each time block is color coordinated in relation to the current time of day (past hours are denoted with a gray color, the present hour is denoted by a red color, and future hours are denoted with a green color). This gives a nice visual representation to each user of where they are within their work day. It allows each user to save events within each time block to local storage which allows their events to persist on page reload. This way, they will not loose their scheduled events if they leave the work day scheduler page. Through building this application I learned the importance of structuring your html in an organized way so that implementing your dynamic elements with JavaScript or JQuery can be made much easier. I also learned by application of conditionals, iteration, and event listeners how to save data to local storage, and then how to re-append that data to the page when it is reloaded. Something new to me which was implemented on this application was the use of adding CDNs (both DayJS and Moment) to the page so that certain classes persist based on the current time.
+
+## Live URL
+
+https://rmessett15.github.io/Work-Day-Scheduler/
+
+## Screenshot
+
+![Screenshot-Weekly-Challenge-5](https://user-images.githubusercontent.com/120127903/224795456-a4266236-8b3b-46ab-a1af-8b655b0a2912.png)
+
+## Technologies Used
+
+This project utilizes HTML, CSS, JavaScript, Materialize........ Fontawesome.com, Google Fonts, DayJS CDN, and Moment CDN
+
+## Installation
+
+1. Clone the repo:
+   git clone https://github.com/rmessett15/Weather-Dashboard.git
+
+2. Open in VS Code. If you do not have VS code you must install it.
+
+## Credits
+
+Worked with a a variety of students to complete this challenge (ie. Brian Hamlin & Chris McNamara), as well as a friend within the developer community (Brian Dillman). My tutoring session with Phillip Loy helped finalize re-appending my locally stored data back to the page.
+
+## Features
+
+Features of this page include the users ability to save added events to local storage and the application re-appending the saved data to the application on page reload. It also utilizes two different CDN's to display the current time which were utilized and then passed through an iteration and conditional statement giving rise to the color coordinated time blocks the user sees.
+
+## How to Contribute
+
+Open to collaboration, if you choose to do so open an issue and modify any changes you would like to see on a feature branch and wait for approval before merging to the main branch.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 // Tries to grab values from local storage and if there is no data, JSON parse will fail, and it will use an empty list instead as backup
+// Want to parse data so data persists across multiple page loads
 var recentSearches = JSON.parse(localStorage.getItem("searches")) || [];
 console.log(recentSearches);
 // My attempt to add to recently viewed
 var searchHistory = document.getElementById("recently-viewed");
-searchHistory.textContent = recentSearches;
 
 function getWeather(city) {
-    var url = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=011d8600955301988250a993be42df9e";
+    var url = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=011d8600955301988250a993be42df9e&units=imperial";
     fetch(url)
     .then(function(response) {
         return response.json();
@@ -14,24 +14,42 @@ function getWeather(city) {
     .then(function (data) {
         console.log(data);
         localStorage.setItem("city", JSON.stringify(data));
+        console.log(data);
         setCurrentWeather(data.list[0]);
         setForecast(data.list);
     })
 }
 
+// Convert kelvin to fahrenheit
+function convertToFahrenheit(kelvin) {
+    var kTemp = kelvin;
+    var kToFar = (kTemp - 273.15) * 1.8 + 32;
+    var message = kToFar + " \xB0F";
+    return message;
+}
+
+console.log(convertToFahrenheit(279.45));
+
 function setCurrentWeather(weather) {
-    console.log(weather.main.temp);
+    // Set current city
+    var currentCity = document.getElementById("current-city");
+    currentCity.textContent = "City " + city.name;
+
     // To set current temp
+    console.log(weather.main.temp);
+    // var kel = parseInt(weather.main.temp);
+    // console.log(kel);
     var currentTemperature = document.getElementById("current-temperature");
-    currentTemperature.textContent = "Temperature: " + weather.main.temp;
-    console.log(weather.main.humidity);
+    var convert = convertToFahrenheit(weather.main.temp);
+    console.log(convert);
+    currentTemperature.textContent = "Temperature: " + weather.main.temp + " \xB0F";
     // To set current wind
     var currentWind = document.getElementById("current-wind");
-    currentWind.textContent = "Wind: " + weather.wind.speed;
+    currentWind.textContent = "Wind: " + weather.wind.speed + " MPH";
     // To set current humidity
     var currentHumidity = document.getElementById("current-humidity");
-    currentHumidity.textContent = "Humidity: " + weather.main.humidity;
-}
+    currentHumidity.textContent = "Humidity: " + weather.main.humidity + " %";
+    }
 
 function setForecast(forecast) {
     for (var i = 0; i < forecast.length; i += 8) {
@@ -41,36 +59,50 @@ function setForecast(forecast) {
 }
 
 function setForecastDay(weather, dayNumber) {
-    // 1. Get the div element for the day (using ID)
-    // 2. Create an array for the new elements that you will add to the day
-    // 3. For each part of the weather, create a new element for it. This involves creating the element, and setting the text
-    // 4. Replace the existing children in the day element with the new children
-    var day = document.getElementById("day-" + dayNumber)
-    var dayList = [];
-    var titleParagraph = document.createElement("p");
-    titleParagraph.textContent = "Day " + dayNumber;
-    dayList.push(titleParagraph);
-    // Date
-    // Icon
-    // Temp
-    var tempParagraph = document.createElement("p");
-    tempParagraph.textContent = "Temp: " + weather.main.temp;
-    dayList.push(tempParagraph);
-    day.replaceChildren(...dayList);
-    // Wind
-    var windParagraph = document.createElement("p");
-    windParagraph.textContent = "Wind: " + weather.wind.speed;
-    dayList.push(windParagraph);
-    day.replaceChildren(...dayList);
-    // Humidity
-    var humidityParagraph = document.createElement("p");
-    humidityParagraph.textContent = "Humidity: " + weather.main.humidity;
-    dayList.push(humidityParagraph);
-    day.replaceChildren(...dayList);
-    // Icon ??????????
-    var iconParagraph = document.createElement("p");
-    iconParagraph.textContent = "Icon: " + weather.weather[0];
-    day.replaceChildren(...dayList);
+  // 1. Get the div element for the day (using ID)
+  // 2. Create an array for the new elements that you will add to the day
+  // 3. For each part of the weather, create a new element for it. This involves creating the element, and setting the text
+  // 4. Replace the existing children in the day element with the new children
+
+  // Icon
+  var iconParagraph = document.createElement("p");
+  var iconParagraphId = weather.weather[0].icon;
+  var iconLink =
+    "https://openweathermap.org/img/wn/" + iconParagraphId + ".png";
+  console.log(iconLink);
+  var iconHTML = '<img src="' + iconLink + '">';
+  console.log(iconHTML);
+
+  // Date
+  var forecastParagraph = document.createElement("p");
+  var forecastDate = weather.dt;
+  console.log(forecastDate);
+  var convertTimeMilli = forecastDate * 1000;
+  var dateTime = new Date(convertTimeMilli);
+  var newDate = dateTime.toLocaleDateString("en-US", { dateStyle: "short" });
+  console.log(newDate);
+  // Day #
+  var day = document.getElementById("day-" + dayNumber);
+  var dayList = [];
+  var titleParagraph = document.createElement("p");
+  console.log(iconHTML);
+  titleParagraph.innerHTML = newDate + iconHTML;
+  dayList.push(titleParagraph);
+  // Temp
+  var tempParagraph = document.createElement("p");
+  tempParagraph.textContent = "Temp: " + weather.main.temp + " \xB0F";
+  dayList.push(tempParagraph);
+  day.replaceChildren(...dayList);
+  // Wind
+  var windParagraph = document.createElement("p");
+  windParagraph.textContent = "Wind: " + weather.wind.speed + " MPH";
+  dayList.push(windParagraph);
+  day.replaceChildren(...dayList);
+  // Humidity
+  var humidityParagraph = document.createElement("p");
+  humidityParagraph.textContent = "Humidity: " + weather.main.humidity + " %";
+  dayList.push(humidityParagraph);
+  day.replaceChildren(...dayList);
 }
 
 var searchForm = document.getElementById("search-form");
@@ -78,10 +110,31 @@ var city = document.getElementById("city");
 searchForm.addEventListener("submit", function(event) {
     event.preventDefault();
     var searchCity = city.value.trim()
+    executeSearch(searchCity);
+});
+
+function addRecentSearch(city) {
+    // 1. Create a function for appending to recently viewed, which is run when a search is run
+    // 2. In that function create a button element with the text of the search and append it to the recent searches list. Store this button in a variable
+    // 3. Add an event listener for when that button is clicked. When it's clicked perform the search for that city
+    // 4. Append that button to the recent searches
+    var recentButton = document.createElement("button");
+    recentButton.textContent = city;
+    recentButton.addEventListener("click", function() {
+        executeSearch(city);
+    });
+    searchHistory.appendChild(recentButton);
+}
+
+function executeSearch(searchCity) {
     recentSearches.push(searchCity);
     localStorage.setItem("searches", JSON.stringify(recentSearches));
+    addRecentSearch(searchCity);
     getWeather(searchCity);
-});
+}
+
+
+
 
 
 
@@ -131,6 +184,10 @@ searchForm.addEventListener("submit", function(event) {
 // Need to style page
 // Need to format all docs
 // Need to complete README.md
+
+// Why do I stringify and parse in regards to putting in and pulling out of local storage
+
+
 
 
 
